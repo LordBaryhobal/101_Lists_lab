6aff5ee3107c2475d3eb6a544bc900d66a3e1bdc
LordBaryhobal <lordbaryhobal@gmail.com>
2024-03-26 13:41:41 +0100

    task 3

---
+++ src/main/scala/Task3.scala
@@ -0,0 +1,8 @@
+object Task3 {
+  def main(args: Array[String]): Unit = {
+    val n3: Node = new Node("Milan", null)
+    val n2: Node = new Node("Paris", n3)
+    val n1: Node = new Node("Tokyo", n2)
+
+  }
+}
